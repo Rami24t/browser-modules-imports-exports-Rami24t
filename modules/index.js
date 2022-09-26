7cba101f0ffe820784892679b08abb402478dc84
RamS24 <AlSaadi.Rami24@gmail.com>
2022-09-26 16:39:41 +0200

    solution

--- modules/index.js
+++ modules/index.js
@@ -1,1 +1,24 @@
-// Your code goes here!
+import { calculateAspectRatio, fixRoundingErrors } from "./aspect-ratio.js";
+import { modulo, percentage, percentageOf, difference } from "./percentage.js";
+
+document.addEventListener('input', (e) => {
+    console.log(e.target.id);
+    if (e.target.id.startsWith('modulo')) {
+        document.getElementById('modulo_result').value = modulo(document.getElementById('modulo_1').value, document.getElementById('modulo_2').value);
+    }
+    if (e.target.id.startsWith('percentage')) {
+        document.getElementById('percentage_result').value = percentage(document.getElementById('percentage_1').value, document.getElementById('percentage_2').value);
+    }
+    if (e.target.id.startsWith('percentageOf')) {
+        document.getElementById('percentageOf_result').value = percentageOf(document.getElementById('percentageOf_1').value, document.getElementById('percentageOf_2').value);
+    }
+    if (e.target.id.startsWith('difference')) {
+        document.getElementById('difference_result').value = difference(document.getElementById('difference_1').value, document.getElementById('difference_2').value);
+    }
+    if (e.target.id.startsWith('ratio_result-w')) {
+        document.getElementById('ratio_result-height').value = calculateAspectRatio(document.getElementById('ratio_1').value, document.getElementById('ratio_2').value, document.getElementById('ratio_result-width').value, 'w');
+    }
+    if (e.target.id.startsWith('ratio_result-h')) {
+        document.getElementById('ratio_result-width').value = calculateAspectRatio(document.getElementById('ratio_1').value, document.getElementById('ratio_2').value, document.getElementById('ratio_result-height').value, 'h');
+    }
+});
